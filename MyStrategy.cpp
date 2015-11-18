44569cc770e54ae35fb492dfb8bd39b187376528
Alexandr Nosov <anosov@ivc.nnov.ru>
2015-11-18 10:16:06 +0400

    append structure MyStrategy::Impl

--- MyStrategy.cpp
+++ MyStrategy.cpp
@@ -5,14 +5,46 @@
 
 #include <cmath>
 #include <cstdlib>
+#include <iostream>
 
 using namespace model;
 using namespace std;
 
+struct MyStrategy::Impl {
+    World const* world;
+    Impl() {}
+    void updateWorld(World const* world_) { if(world != world_) world = world_; }
+
+    void showTilesXY() const {
+        cout << "TilesXY" << endl;
+        auto const& tilesXY = world->getTilesXY();
+        for(int j=0; j<world->getHeight(); j++) {
+            for(int i=0; i<world->getWidth(); i++) {
+                cout << tilesXY[i][j];
+            }
+            cout << endl;
+        }
+        cout << endl;
+
+//        for(auto const& tile : world->getWaypoints())
+    }
+};
+
+
 void MyStrategy::move(const Car& self, const World& world, const Game& game, Move& move) {
+    impl->updateWorld(&world);
+    impl->showTilesXY();
+
+    Car const& car = self;
+    int px = static_cast<int>(car.getX() / 800);
+    int py = static_cast<int>(car.getY() / 800);
+    cout << px << " " << py << endl;
+    cout << world.getTilesXY()[px][py+1] << endl;
+
+    exit(1);
 //    move.setSpeedUp(-1.0);
 //    move.setTurn(PI);
 //    move.setAction(STRIKE);
 }
 
-MyStrategy::MyStrategy() { }
+MyStrategy::MyStrategy() : impl(new Impl) { }
--- MyStrategy.h
+++ MyStrategy.h
@@ -4,9 +4,13 @@
 #define _MY_STRATEGY_H_
 
 #include "Strategy.h"
+#include <memory>
 
 class MyStrategy : public Strategy {
 public:
+    class Impl;
+    std::shared_ptr<Impl> impl;
+
     MyStrategy();
 
     void move(const model::Car& self, const model::World& world, const model::Game& game, model::Move& move) override;
